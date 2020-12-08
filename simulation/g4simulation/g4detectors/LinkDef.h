92c0ad80219a852192b5da278929da333a50d3ea
Dmitri Smirnov <d.s@plexoos.com>
2020-12-08 16:33:04 -0500

    [cmake] Initial support for g4detectors
    Combine all LinkDef's into a single file
    
    cat simulation/g4simulation/g4detectors/*LinkDef.h > simulation/g4simulation/g4detectors/LinkDef.h
    
    g4detectors

---
+++ simulation/g4simulation/g4detectors/LinkDef.h
@@ -0,0 +1,163 @@
+#ifdef __CINT__
+
+#pragma link C++ class PHG4BbcSubsystem-!;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4BlockCellGeomContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4BlockCellGeom+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4BlockCellGeomv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4BlockGeomContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4BlockGeom+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4BlockGeomv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CellContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Cell+;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Cellv1+;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Cellv2+;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderCellContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderCellGeomContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderCellGeom+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderCellGeom_Spacalv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderCell+;
+#pragma link C++ namespace PHG4CylinderCellDefs-!;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderCellv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderCellv2+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderCellv3+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeomContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeom+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeom_Spacalv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeom_Spacalv2+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeom_Spacalv3+;
+#pragma link C++ class PHG4CylinderGeom_Spacalv3::geom_tower+;
+#pragma link C++ class PHG4CylinderGeom_Spacalv3::scint_id_coder+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeomv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeomv2+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeomv3+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4CylinderGeomv4+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4ScintillatorSlatContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4ScintillatorSlatDefs-!;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4ScintillatorSlat+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4ScintillatorSlatv1+;
+
+#endif /* __CINT__ */
