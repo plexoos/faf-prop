40b4f55dba64089b4f41e47c2054a570e985740a
Dmitri Smirnov <d.s@plexoos.com>
2020-12-09 17:37:58 -0500

    [cmake] Initial support for g4main
    Combine all LinkDef's into a single file
    
    cat simulation/g4simulation/g4main/*LinkDef.h > simulation/g4simulation/g4main/LinkDef.h

---
+++ simulation/g4simulation/g4main/LinkDef.h
@@ -0,0 +1,90 @@
+#if defined(__CLING__)
+
+#pragma link C++ class EicEventHeader+;
+
+#endif /* defined(__CLING__) */
+#if defined(__CLING__)
+
+#pragma link C++ class EicEventHeaderv1+;
+
+#endif /* defined(__CLING__) */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4EventHeader+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4EventHeaderv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4HitContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4HitEval+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Hit+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Hitv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4InEvent+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Particle + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Particlev1 + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Particlev2 + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Particlev3 + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Shower+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4Showerv1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4TruthInfoContainer+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4VtxPoint+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHG4VtxPointv1+;
+
+#endif /* __CINT__ */
