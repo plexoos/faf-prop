5d408844c7727bfa35ad8e91fe01679f9eb18451
Dmitri Smirnov <d.s@plexoos.com>
2020-12-16 20:16:15 -0500

    [cmake] Initial support for g4bbc

---
+++ simulation/g4simulation/g4bbc/LinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+
+#pragma link C++ class BbcVertex + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class BbcVertexMap + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class BbcVertexMapv1 + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class BbcVertexv1 + ;
+
+#endif /* __CINT__ */
