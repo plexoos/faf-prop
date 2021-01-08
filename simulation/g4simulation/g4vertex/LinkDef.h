e9dc8d0481b0124dd6215c9287a8d69c7d126d0f
Dmitri Smirnov <dmixsmi@gmail.com>
2021-01-08 18:19:42 -0500

    [cmake] Initial support for g4vertex
    Combine all LinkDef's into a single file
    
    cat simulation/g4simulation/g4vertex/*LinkDef.h > simulation/g4simulation/g4vertex/LinkDef.h

---
+++ simulation/g4simulation/g4vertex/LinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+
+#pragma link C++ class GlobalVertex + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class GlobalVertexMap + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class GlobalVertexMapv1 + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class GlobalVertexv1 + ;
+
+#endif /* __CINT__ */
