6dad86807a28f3f5ca6f25b8e5093c35a0fabeb8
Dmitri Smirnov <dmixsmi@gmail.com>
2021-01-14 13:58:35 -0500

    [cmake] Initial support for trackreco

---
+++ offline/packages/trackreco/LinkDef.h
@@ -0,0 +1,35 @@
+#ifdef __CINT__
+
+#pragma link C++ class AssocInfoContainer + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class HelixHoughBin+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class HelixHoughBin_v1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+ 
+#pragma link C++ class HelixHoughFuncs+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class HelixHoughFuncs_v1+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+ 
+#pragma link C++ class HelixHoughSpace+;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class HelixHoughSpace_v1+;
+
+#endif /* __CINT__ */
