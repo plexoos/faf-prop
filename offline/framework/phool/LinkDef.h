0799bd9bd5fc6e7431dd657a035a122d1b869329
Dmitri Smirnov <dmixsmi@gmail.com>
2021-03-17 22:06:49 -0400

    [cmake] Initial support for phool

---
+++ offline/framework/phool/LinkDef.h
@@ -0,0 +1,18 @@
+#ifdef __CINT__
+
+#pragma link C++ class PHObject + ;
+
+#endif /* __CINT__ */
+#ifdef __CINT__
+
+#pragma link C++ class PHTimeStamp + ;
+
+#ifndef __CLING__
+#pragma link C++ function operator+ (const PHTimeStamp &, time_t);
+#pragma link C++ function operator- (const PHTimeStamp &, time_t);
+#pragma link C++ function operator- (const PHTimeStamp &, const PHTimeStamp &);
+#pragma link C++ function operator<< (ostream &, const PHTimeStamp &);
+#pragma link C++ function operator>> (istream &, const PHTimeStamp &);
+#endif
+
+#endif /* __CINT__ */
