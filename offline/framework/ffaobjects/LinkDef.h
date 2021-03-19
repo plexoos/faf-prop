4cf3e0e0de4fb15d8d158095235de6fe97b64f5c
Dmitri Smirnov <dmixsmi@gmail.com>
2021-03-18 20:00:15 -0400

    [cmake] Initial support for ffaobjects

---
+++ offline/framework/ffaobjects/LinkDef.h
@@ -0,0 +1,45 @@
+#ifdef __CINT__
+
+#pragma link C++ class EventHeader + ;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class EventHeaderv1 + ;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class EventHeaderv2 + ;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class FlagSave + ;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class FlagSavev1 + ;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class RunHeader + ;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class RunHeaderv1 + ;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class SyncObject + ;
+
+#endif
+#ifdef __CINT__
+
+#pragma link C++ class SyncObjectv1 + ;
+
+#endif
